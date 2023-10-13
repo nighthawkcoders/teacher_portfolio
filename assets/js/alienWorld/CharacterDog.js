a6e3bf685ff58b3c1b7eec89942e3d5b35bb3ed0
John Mortensen <jmortensen@powayusd.com>
2023-10-13 05:47:49 -0700

    Rename class to abide with standards

--- assets/js/alienWorld/CharacterDog.js
+++ assets/js/alienWorld/CharacterDog.js
@@ -1,6 +1,6 @@
 import Character from './Character.js';
 
-const Dog_Animation = {
+const DogAnimation = {
     // Sprite properties
     scale: 1,
     width: 160,
@@ -10,22 +10,22 @@ const Dog_Animation = {
 	walking: { row: 2, frames: 47 }
 }
 
-export class Character_Dog extends Character{
+export class CharacterDog extends Character{
     // constructors sets up Character object 
     constructor(dogCanvas, image, gameSpeed, speedRatio){
         super(dogCanvas, 
             image, 
             gameSpeed, 
             speedRatio,
-            Dog_Animation.width, 
-            Dog_Animation.height, 
-            Dog_Animation.scale
+            DogAnimation.width, 
+            DogAnimation.height, 
+            DogAnimation.scale
         );
     }
 
     // Dog perform a unique update
     update() {
-        if (this.frameY === Dog_Animation.walking.row) {
+        if (this.frameY === DogAnimation.walking.row) {
             this.x -= this.speed;  // Move the dog to the left
             // Check if the dog has moved off the left edge of the canvas
             if (this.x < -this.canvas.width) {
@@ -45,11 +45,11 @@ export class Character_Dog extends Character{
 // In this case the dog is following the default character initialization
 export function initDog(canvasId, image, gameSpeed, speedRatio, controls){
     // Create the Dog character
-    var dog = new Character_Dog(canvasId, image, gameSpeed, speedRatio);
+    var dog = new CharacterDog(canvasId, image, gameSpeed, speedRatio);
 
     // Dog Frame position and Frame extents
-    dog.setFrameY(Dog_Animation.walking.row);
-    dog.setMaxFrame(Dog_Animation.walking.frames);
+    dog.setFrameY(DogAnimation.walking.row);
+    dog.setMaxFrame(DogAnimation.walking.frames);
 
     // Dog Screen Position
     dog.setX(window.innerWidth);
@@ -63,8 +63,8 @@ export function initDog(canvasId, image, gameSpeed, speedRatio, controls){
     controls.addEventListener('click', function (event) {
         if (event.target.tagName === 'INPUT') {
             const selectedAnimation = event.target.id;
-            dog.setFrameY(Dog_Animation[selectedAnimation].row);
-            dog.setMaxFrame(Dog_Animation[selectedAnimation].frames);
+            dog.setFrameY(DogAnimation[selectedAnimation].row);
+            dog.setMaxFrame(DogAnimation[selectedAnimation].frames);
         }
     });
 
@@ -72,4 +72,4 @@ export function initDog(canvasId, image, gameSpeed, speedRatio, controls){
     return dog;
 }
 
-export default Character_Dog;
+export default CharacterDog;
