6415883176305cdde34a4556b1e4914b7af7e887
John Mortensen <jmortensen@powayusd.com>
2023-10-17 20:49:44 -0700

    move all canvas elements to canvas container, oops

--- assets/js/alienWorld/CharacterMonkey.js
+++ assets/js/alienWorld/CharacterMonkey.js
@@ -54,7 +54,7 @@ export function initMonkey(canvasId, image, gameSpeed, speedRatio){
 
     // Monkey Screen Position
     monkey.setX(GameEnv.innerWidth);
-    monkey.setY(GameEnv.bottom * .92);
+    monkey.setY(GameEnv.bottom * 1);
 
     /* Monkey Control 
     * changes y value, the row in sprite
