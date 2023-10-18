3c9de62a2a6fce79203dd5b3a75c6a31710a8e66
John Mortensen <jmortensen@powayusd.com>
2023-10-18 06:11:50 -0700

    resize canvas

--- assets/js/alienWorld/Background.js
+++ assets/js/alienWorld/Background.js
@@ -12,31 +12,22 @@ export class Background extends GameObject {
         this.ctx.drawImage(this.image, this.x, this.y);
         this.ctx.drawImage(this.image, this.x + this.width, this.y);
     }
-}
+    size() {
+        // Update canvas size
+        const ADJUST = 1.42 // visual layer adjust, use "1"" for a perfect image 
 
-export function initBackground(canvas, image, speedRatio) {
-    // Build game object
-    var background = new Background(canvas, image, speedRatio);
+        const canvasWidth = GameEnv.innerWidth;
+        const canvasHeight = canvasWidth / this.aspect_ratio;
+        const canvasLeft = 0;
 
-    // Setup background constant to adjust display size
-    const ADJUST = 1.42 // visual layer adjust, use "1"" for a perfect loop 
-
-    // Set Dimensions to match the image width
-    const canvasWidth = GameEnv.innerWidth;
-    const canvasHeight = canvasWidth / background.aspect_ratio;  // height is oriented by width
-    const canvasLeft = 0; // Start image from the left edge horizontally
-
-    // Set dimensions for the background canvas
-    canvas.width = background.width / ADJUST;
-    canvas.height = background.height / ADJUST;
-    // Set Style properties for the background canvas
-    canvas.style.width = `${canvasWidth}px`;
-    canvas.style.height = `${canvasHeight}px`;
-    canvas.style.position = 'absolute';
-    canvas.style.left = `${canvasLeft}px`;
-    canvas.style.top = `${GameEnv.top}px`;
-
-    return background;
+        this.canvas.width = this.width / ADJUST;
+        this.canvas.height = this.height / ADJUST;
+        this.canvas.style.width = `${canvasWidth}px`;
+        this.canvas.style.height = `${canvasHeight}px`;
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.left = `${canvasLeft}px`;
+        this.canvas.style.top = `${GameEnv.top}px`;
+    }
 }
 
 export default Background;
