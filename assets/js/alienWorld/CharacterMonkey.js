33c62749587762e3f93aa764d7a0579eae2e7418
John Mortensen <jmortensen@powayusd.com>
2023-10-18 10:55:54 -0700

    proportional monkey

--- assets/js/alienWorld/CharacterMonkey.js
+++ assets/js/alienWorld/CharacterMonkey.js
@@ -45,9 +45,14 @@ export class CharacterMonkey extends Character{
         this.setFrameX(MonkeyAnimation['a'].idleFrame.column)
         this.setMaxFrame(MonkeyAnimation['a'].idleFrame.frames);
 
-        // Monkey Screen Position
-        this.setX(GameEnv.innerWidth);
-        this.setY(GameEnv.bottom * 1);
+        if (GameEnv.prevInnerWidth) {
+            const proportionalX = (this.x / GameEnv.prevInnerWidth) * GameEnv.innerWidth;
+            const proportionalY = (this.y / GameEnv.prevBottom) * GameEnv.bottom;
+        } else {
+            // Monkey Screen Position
+            this.setX(GameEnv.innerWidth);
+            this.setY(GameEnv.bottom * 1);
+        }
     }
 }
 
