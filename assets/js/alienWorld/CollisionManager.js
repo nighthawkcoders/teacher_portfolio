dedd04c160a169b17815db5a60533a4a82954dee
sl984 <shane.lopez100@gmail.com>
2023-10-19 10:52:50 -0700

    Added comments to the collision manager

--- assets/js/alienWorld/CollisionManager.js
+++ assets/js/alienWorld/CollisionManager.js
@@ -9,10 +9,14 @@ export class CollisionManager{
     }
 
     checkForCollisions(){
+        // Go through every game object stored in our gameObjectList to see if any of them overlap
         for (var thisObject of this.gameObjectList){
+            // For our second loop we only want to compare everything after this object to avoid comaparing 
+            // objects twice and so we don't compare the object to itself
             var thisObjectIndex = this.gameObjectList.indexOf(thisObject);
             var subGameObjectList = this.gameObjectList.slice(thisObjectIndex + 1);
             for (var otherObject of subGameObjectList){
+                // If the two objects are colliding then inform the two objects that they have been in a collision
                 var collisionData = thisObject.isCollision(otherObject);
                 if (collisionData.hit){
                     document.dispatchEvent(
