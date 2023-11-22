956383c903f2fb375e3fd526156f6e16bf74c324
jm1021 <jmortensen@powayusd.com>
2023-11-22 14:20:35 -0800

    tube again

--- assets/js/mario/Tube.js
+++ assets/js/mario/Tube.js
@@ -23,7 +23,7 @@ export class Tube extends GameObject {
         const scaledWidth = scaledHeight * this.aspect_ratio;
         const bottom = (GameEnv.bottom - scaledHeight);
         const tubeX = .80 * GameEnv.innerWidth;
-        const tubeY = bottom + (bottom * .03);  // tube is not at bottom of image
+        const tubeY = bottom + (bottom * .025);  // tube is not at bottom of image
 
         // set variables used in Display and Collision algorithms
         this.bottom = tubeY;
