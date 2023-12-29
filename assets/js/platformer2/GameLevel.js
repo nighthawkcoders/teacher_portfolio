af8e28b296ca49aeeff204f6d308b7aaf484952f
jm1021 <jmortensen@powayusd.com>
2023-12-29 11:01:35 -0800

    rework code, eliminate duplication

--- assets/js/platformer2/GameLevel.js
+++ assets/js/platformer2/GameLevel.js
@@ -8,143 +8,54 @@ import Player from './Player.js';
 import Tube from './Tube.js';
 import Goomba from './Goomba.js';
 
-// Store the assets and attributes of the Game at the specific GameLevel.
 class GameLevel {
     constructor(gameObject) {
-        // conditional assignments from GameObject to instance variables
-        this.tag = gameObject?.tag;
-        this.backgroundImg = gameObject.background?.file;
-        this.backgroundImgHills = gameObject.backgroundHills?.file;
-        this.backgroundImgMountains = gameObject.backgroundMountains?.file;
-        this.platformImg = gameObject.platform?.file;
-        this.jumpPlatformImg = gameObject.jumpPlatform?.file;
-        this.playerImg = gameObject.player?.file;
-        this.playerData = gameObject?.player;
-        this.tubeImg = gameObject.tube?.file;
-        this.goombaImg = gameObject.goomba?.file;
-        this.goombaData = gameObject?.goomba;
+        // order objects are loaded is important
+        this.gameObjects = [
+            { name: 'backgroundMountains', class: BackgroundMountains, id: 'background', speedRatio: 0 },
+            { name: 'backgroundHills', class: BackgroundHills, id: 'background', speedRatio: 0 },
+            { name: 'background', class: Background, id: 'background', speedRatio: 0 },
+            { name: 'platform', class: Platform, id: 'platform', speedRatio: 0 },
+            { name: 'jumpPlatform', class: JumpPlatform, id: 'jumpPlatform', speedRatio: 0 },
+            { name: 'player', class: Player, id: 'player', speedRatio: 0.7 },
+            { name: 'tube', class: Tube, id: 'tube', speedRatio: 0 },
+            { name: 'goomba', class: Goomba, id: 'goomba', speedRatio: 0.7 },
+        ];
+        
+        this.tag = gameObject?.tag; // friendly name used to identify level
         this.isComplete = gameObject?.callback; // function that determines if level is complete
+
+        // associate data with each gameObject
+        this.gameObjects.forEach(obj => {
+            obj.file = gameObject[obj.name]?.file;
+            obj.data = gameObject[obj.name];
+        });
+
+        // store GameLevel instance in GameEnv
         GameEnv.levels.push(this);
     }
 
-    // Load level data
+    // load each image and create a new instance of the game element
     async load() {
-        
-        // test for presence of Images
-        const imagesToLoad = [];
-        if (this.backgroundImgMountains) {
-            imagesToLoad.push(this.loadImage(this.backgroundImgMountains));
-        }
-        if (this.backgroundImgHills) {
-            imagesToLoad.push(this.loadImage(this.backgroundImgHills));
-        }
-        if (this.backgroundImg) {
-            imagesToLoad.push(this.loadImage(this.backgroundImg));
-        }
-        if (this.platformImg) {
-            imagesToLoad.push(this.loadImage(this.platformImg));
-        }
-        if (this.jumpPlatformImg) {
-            imagesToLoad.push(this.loadImage(this.jumpPlatformImg));
-        }
-        if (this.playerImg) {
-            imagesToLoad.push(this.loadImage(this.playerImg));
-        }
-        if (this.tubeImg) {
-            imagesToLoad.push(this.loadImage(this.tubeImg));
-        }
-        if (this.goombaImg) {
-            imagesToLoad.push(this.loadImage(this.goombaImg));
-        }
-
         try {
-            // Do not proceed until images are loaded
-            const loadedImages = await Promise.all(imagesToLoad);
-            var i = 0;
-
-            if (this.backgroundImgMountains) {
-                const backgroundCanvas = document.createElement("canvas");
-                backgroundCanvas.id = "background";
-                document.querySelector("#canvasContainer").appendChild(backgroundCanvas);
-                const backgroundSpeedRatio = 0;
-                new BackgroundMountains(backgroundCanvas, loadedImages[i], backgroundSpeedRatio);
-                i++;
+            for (const obj of this.gameObjects) {
+                if (obj.file) {
+                    // load image
+                    obj.image = await this.loadImage(obj.file);
+                    const canvas = document.createElement("canvas");
+                    canvas.id = obj.id;
+                    document.querySelector("#canvasContainer").appendChild(canvas);
+                    // game element instance
+                    new obj.class(canvas, obj.image, obj.speedRatio, obj.data);
+                }
             }
-
-            if (this.backgroundImgHills) {
-                const backgroundCanvas = document.createElement("canvas");
-                backgroundCanvas.id = "background";
-                document.querySelector("#canvasContainer").appendChild(backgroundCanvas);
-                const backgroundSpeedRatio = 0;
-                new BackgroundHills(backgroundCanvas, loadedImages[i], backgroundSpeedRatio);
-                i++;
-            }
-
-            // Prepare HTML with Background Canvas (if backgroundImg is defined)
-            if (this.backgroundImg) {
-                const backgroundCanvas = document.createElement("canvas");
-                backgroundCanvas.id = "background";
-                document.querySelector("#canvasContainer").appendChild(backgroundCanvas);
-                const backgroundSpeedRatio = 0;
-                new Background(backgroundCanvas, loadedImages[i], backgroundSpeedRatio);
-                i++;
-            }
-
-            // Prepare HTML with Platform Canvas
-            if (this.platformImg) {
-                const platformCanvas = document.createElement("canvas");
-                platformCanvas.id = "platform";
-                document.querySelector("#canvasContainer").appendChild(platformCanvas);
-                const platformSpeedRatio = 0;
-                new Platform(platformCanvas, loadedImages[i], platformSpeedRatio);
-                i++;
-            }
-
-            if (this.jumpPlatformImg) {
-                const platformCanvas = document.createElement("canvas");
-                platformCanvas.id = "jumpPlatform";
-                document.querySelector("#canvasContainer").appendChild(platformCanvas);
-                const platformSpeedRatio = 0;
-                new JumpPlatform(platformCanvas, loadedImages[i], platformSpeedRatio);
-                i++;
-            }
-            
-            // Prepare HTML with Player Canvas
-            if (this.playerImg) {
-                const playerCanvas = document.createElement("canvas");
-                playerCanvas.id = "player";
-                document.querySelector("#canvasContainer").appendChild(playerCanvas);
-                const playerSpeedRatio = 0.7;
-                new Player(playerCanvas, loadedImages[i], playerSpeedRatio, this.playerData);
-                i++;
-            }
-
-            // Prepare HTML with Player Canvas
-            if (this.tubeImg) {
-                const tubeCanvas = document.createElement("canvas");
-                tubeCanvas.id = "tube";
-                document.querySelector("#canvasContainer").appendChild(tubeCanvas);
-                new Tube(tubeCanvas, loadedImages[i]);
-                i++;
-            }
-            
-            // Prepare HTML with goomba Canvas
-            if (this.goombaImg) {
-                const goombaCanvas = document.createElement("canvas");
-                goombaCanvas.id = "goomba";
-                document.querySelector("#canvasContainer").appendChild(goombaCanvas);
-                const goombaSpeedRatio = 0.7;
-                new Goomba(goombaCanvas, loadedImages[i], goombaSpeedRatio, this.goombaData);
-                i++;
-            }
-
+        // halt game if any images fail to load
         } catch (error) {
             console.error('Failed to load one or more images:', error);
         }
-
     }
 
-    // Create a function to load an image and return a Promise
+    // image loader
     async loadImage(src) {
         return new Promise((resolve, reject) => {
             const image = new Image();
